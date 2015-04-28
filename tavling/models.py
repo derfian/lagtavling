c02319b77a0086beaeea4b360a6090137797e1f7
Karl Mikaelsson <derfian@lysator.liu.se>
2015-04-28 21:18:43 +0200

    Add basic models for races

--- tavling/models.py
+++ tavling/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Tavling(models.Model):
+    TAVLING_TYPES = ((u'AG', u'Agilityklass'),
+                     (u'HO', u'Hoppklass'))
+    namn = models.CharField(max_length=200)
+    typ = models.CharField(max_length=2, choices=TAVLING_TYPES)
+
+class Lag(models.Model):
+    tavling = models.ManyToManyField(Tavling, related_name='lag')
+    ekipage = models.ManyToManyField(Ekipage, related_name='lag')
+
+class Ekipage(models.Model):
+    hundid = models.CharField(max_length=25)
+    hundnamn = models.CharField(max_length=50)
+    hundras = models.CharField(max_length=50)
+    forare = models.CharField(max_length=50)
+
